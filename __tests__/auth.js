00b2a7ee60c85dceb5c543348e9106fc4d8d3963
RONIE LIMA FERNANDES <roneylf@gmail.com>
2020-05-28 22:23:03 -0300

    Create auth.js

---
+++ __tests__/auth.js
@@ -0,0 +1,74 @@
+
+import { AsyncStorage } from 'react-native'
+export const LOG_IN = "LOG_IN"
+export const START_UP = "START_UP"
+export const LOG_OUT = "LOG_OUT"
+export const AUTHENTICATE = 'AUTHENTICATE'
+
+import config from '../../helpers/config'
+
+let timer;
+
+export const logout = () => {
+    return async dispatch => {
+        clearTimeout(timer);
+        await AsyncStorage.removeItem('userData')
+        dispatch({
+            type: LOG_OUT
+        })
+    }
+}
+const expirationInterval = (timeout) => {
+    if (timeout) {
+        return dispatch => {
+            timer = setTimeout(() => {
+                dispatch(logout());
+            }, timeout * 1000)
+        }
+    }
+}
+export const authenticate = (auth) => {
+    return dispatch => {
+        dispatch({
+            type: AUTHENTICATE,
+            auth: auth
+        })
+    }
+}
+export const login = (email, password) => {
+    return async dispatch => {
+        try {
+            console.log('request')
+            const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${config.apiKey}`, {
+                method: "post",
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                    returnSecureToken: true
+                })
+            })
+            const resData = await response.json();
+            await AsyncStorage.setItem('userData', JSON.stringify({
+                email: resData.email,
+                token: resData.idToken,
+                expiresIn: new Date().getTime() + resData.expiresIn * 1000,
+                userId: resData.localId
+            }))
+            dispatch(expirationInterval(resData.expiresIn))
+            dispatch({
+                type: LOG_IN,
+                data: {
+                    email: resData.email,
+                    token: resData.idToken,
+                    expiresIn: resData.expiresIn,
+                    userId: resData.localId
+                }
+            })
+        } catch (e) {
+            throw new error('Problema no login')
+        }
+    }
+}
